7a4aaeb912a06f6eb26b48ae4beb4e49141f856e
wangningjun <ningjun_wang@163.com>
2018-08-22 16:30:32 +0800

    添加读取系统输入数据的方式

--- read line data.py
+++ read line data.py
@@ -1,9 +1,17 @@
 '''
 牛客网的数据读取方式
-1.循环读取，直到输入空格后退出
+1.循环读取，直到输入空格后退出,使用line
 '''
-import sys
-a = []
-for line in sys.stdin:
-    a.append(line.split())
-    print(a)
+# import sys
+# a = []
+# for line in sys.stdin:
+#     a.append(line.split())
+#     print(a)
+#
+'''
+2.循环读取，使用input()
+'''
+
+n = int(input())
+A = map(int, input().split())
+k, d = map(int, input().split())
